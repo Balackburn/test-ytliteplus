ed3aff5733c4349c378578f68af34f4c0681aa97
Balackburn <allantalbot@protonmail.com>
2024-10-13 11:39:04 +0200

    Added Vite config for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   base: '/test-ytliteplus/', // base URL for GitHub Pages
-  plugins: [],
+  build: {
+    outDir: 'dist'
+  }
 })
